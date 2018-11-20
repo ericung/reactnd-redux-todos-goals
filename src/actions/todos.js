b6075c8b1df5dec5beeb82b42621b99d6857ef32
pietore <ungericw@tutanota.com>
2018-11-19 20:48:51 -0600

    fix bugs

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,6 +1,8 @@
-const ADD_TODO = 'ADD_TODO'
-const REMOVE_TODO = 'REMOVE_TODO'
-const TOGGLE_TODO = 'TOGGLE_TODO'
+import API from 'goals-todos-api'
+
+export const ADD_TODO = 'ADD_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
 
 function addTodo(todo) {
   return {
